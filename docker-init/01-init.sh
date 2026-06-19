4379c19e10c67849ba7d2d3683c623f839d71145
pianosuki <pianosuki@protonmail.com>
2026-06-19 15:00:10 -0300

    Fix database creation init

--- docker-init/01-init.sh
+++ docker-init/01-init.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
 
-psql -v ON_ERROR_STOP=1 --username "postgres" --dbname "graveboards_dev" <<EOF
+psql -v ON_ERROR_STOP=1 --username "postgres" --dbname "postgres" <<EOF
+CREATE DATABASE graveboards_dev;
+CREATE DATABASE graveboards_prod;
 CREATE DATABASE graveboards_test;
 EOF
