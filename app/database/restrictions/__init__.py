54d545bea7fb5f9433d09af259772a865d9f59a1
neto <antoniocruznetoenglish@gmail.com>
2026-07-09 10:57:24 -0300

    Update restrictions package exports for new infrastructure

--- app/database/restrictions/__init__.py
+++ app/database/restrictions/__init__.py
@@ -1,9 +1,27 @@
-from app.database.restrictions.base import RestrictionBase
+from app.database.restrictions.base import RestrictionBase, BeatmapRestrictionBase, DatabaseRestrictionBase
+from app.database.restrictions.context import ExecutionContext
 from app.database.restrictions.exceptions import RestrictionViolationError
-from app.database.restrictions.registry import RESTRICTION_REGISTRY
+from app.database.restrictions.registry import (
+    RESTRICTION_REGISTRY,
+    RESTRICTION_TIERS,
+    get_validator,
+    get_validator_tier,
+    register_validator,
+    get_validators_for_tier,
+    get_supported_versions,
+)
 
 __all__ = [
     "RestrictionBase",
+    "BeatmapRestrictionBase",
+    "DatabaseRestrictionBase",
+    "ExecutionContext",
     "RestrictionViolationError",
     "RESTRICTION_REGISTRY",
+    "RESTRICTION_TIERS",
+    "get_validator",
+    "get_validator_tier",
+    "register_validator",
+    "get_validators_for_tier",
+    "get_supported_versions",
 ]
