8dfcacca8f3d9ecd59d6edfce30dbcc59890738d
pianosuki <pianosuki@protonmail.com>
2026-07-18 06:27:20 +0000

    Fix docker-init to create if not exists

--- docker-init/01-init.sh
+++ docker-init/01-init.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 set -e
 
-psql -v ON_ERROR_STOP=1 --username "postgres" --dbname "postgres" <<EOF
-CREATE DATABASE graveboards_dev;
-CREATE DATABASE graveboards_prod;
-CREATE DATABASE graveboards_test;
-EOF
+for db in graveboards_dev graveboards_prod graveboards_test; do
+    exists=$(psql -v ON_ERROR_STOP=1 --username "postgres" --dbname "postgres" -tAc "SELECT 1 FROM pg_database WHERE datname = '$db'")
+    if [ "$exists" != "1" ]; then
+        psql -v ON_ERROR_STOP=1 --username "postgres" --dbname "postgres" -c "CREATE DATABASE $db"
+    fi
+done
